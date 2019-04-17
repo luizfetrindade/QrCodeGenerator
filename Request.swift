a540cf81ba315a8f1e461c988f640b4b270f7ff5
luizfetrindade <luiz.fetrindade@gmail.com>
2019-04-17 11:28:42 -0300

    Adiciona Projeto

---
+++ Request.swift
@@ -0,0 +1,9 @@
+//
+//  Request.swift
+//  nanoChallengApi
+//
+//  Created by Luiz Felipe Trindade on 16/04/19.
+//  Copyright © 2019 Luiz Felipe Trindade. All rights reserved.
+//
+
+import Foundation
---
+++ nanoChallengApi/CustomTableViewCellToQr.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCellToQr.swift
+//  nanoChallengApi
+//
+//  Created by Luiz Felipe Trindade on 16/04/19.
+//  Copyright © 2019 Luiz Felipe Trindade. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCellToQr: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
