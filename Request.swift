6ef88028e088ed81f2f0e72b29846b387f29dedb
luizfetrindade <luiz.fetrindade@gmail.com>
2019-04-17 11:34:57 -0300

    Adiciona codigos

--- Request.swift
+++ Request.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+import UIKit
+
+struct Request{
+    func makeGetCall(data: String, size: String, completionHandler completion: @escaping (UIImage?) -> Void) {
+        let str: String = "https://api.qrserver.com/v1/create-qr-code/?\(size)=\(size)&data=\(data)"
+        if let url = URL(string: str) {
+            let urlRequest = URLRequest(url: url)
+            let session = URLSession.shared
+            _ = session.dataTask(with: urlRequest) {
+                (data, response, error) in
+                guard error == nil else {
+                    completion(nil)
+                    print(error!)
+                    return
+                }
+                if let responseData = data {
+                    completion(UIImage(data: responseData))
+                    return
+                }
+                }.resume()
+        } else{ completion(nil) }
+    }
+}
--- nanoChallengApi/ViewController.swift
+++ nanoChallengApi/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    @IBOutlet weak var linkTextField: UITextField?
+    @IBOutlet weak var qrCodeView: UIImageView?
+    @IBOutlet weak var doneLabel: UILabel!
+    @IBOutlet weak var loadIcon: UIActivityIndicatorView!
+    
+    @IBOutlet weak var convertButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        convertToQrCodeButton((Any).self)
+        doneLabel.isHidden = true
+        convertButton.layer.cornerRadius = 15
+        convertButton.layer.borderWidth = 0.5
+        convertButton.layer.borderColor = UIColor.white.cgColor
+        loadIcon.isHidden = true
     }
+    
+    
+    @IBAction func convertToQrCodeButton(_ sender: Any) {
+        loadIcon.isHidden = false
+        let req : Request = Request.init()
+        req.makeGetCall(data: (linkTextField?.text)!, size: "30", completionHandler: { (image) in
+            if let img = image{
+                self.qrCodeView?.image = img
+                self.loadIcon.isHidden = true
+            }
+        })
+        if linkTextField?.text != "" {
+            doneLabel.text = "QrCode For: \((linkTextField?.text)!)"
+            doneLabel.isHidden = false
+            linkTextField?.text = nil
+            view.endEditing(true)
+        }
+    }
+    
 
-
+    
+    
+    @IBAction func shareItem(_ sender: UIBarButtonItem) {
+        if qrCodeView?.image != nil{
+            let imageToShare = [ qrCodeView?.image! ]
+            
+            let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+            activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+            
+            activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+            
+            self.present(activityViewController, animated: true, completion: nil)
+        }
+        
+        
+    }
 }
 
